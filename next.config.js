d88706e03e62001bf8c8273a80c951475b426d1b
nandu krishna <k.nandu@whatarage.com>
2023-09-23 20:42:32 +0530

    initial push

---
+++ next.config.js
@@ -0,0 +1,28 @@
+const { withPayload } = require("@payloadcms/next-payload");
+const path = require("path");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = withPayload(
+  {
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    reactStrictMode: true,
+    transpilePackages: ["@payloadcms/plugin-seo"],
+    images: {
+      domains: [
+        "localhost",
+        "nextjs-vercel.payloadcms.com",
+        "rage-payload.vercel.app",
+        "rage-payload-cms.s3.amazonaws.com",
+        process.env.NEXT_PUBLIC_APP_URL,
+        // `${process.env.NEXT_PUBLIC_S3_ENDPOINT}`.replace("https://", ""),
+      ],
+    },
+  },
+  {
+    configPath: path.resolve(__dirname, "./payload/payload.config")
+  }
+);
+
+module.exports = nextConfig;

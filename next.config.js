c3228d35707a967165e06f0b3476bcd590fa9287
nandu krishna <k.nandu@whatarage.com>
2023-09-26 15:37:00 +0530

    next.config aws url

--- next.config.js
+++ next.config.js
@@ -15,6 +15,7 @@ const nextConfig = withPayload(
         "nextjs-vercel.payloadcms.com",
         "rage-payload.vercel.app",
         "rage-payload-cms.s3.amazonaws.com",
+        "main.d2pbkid6j17mz2.amplifyapp.com",
         process.env.NEXT_PUBLIC_APP_URL,
         // `${process.env.NEXT_PUBLIC_S3_ENDPOINT}`.replace("https://", ""),
       ],
